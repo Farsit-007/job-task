87597623cd0f57067b690c1441cbb6df446abc59
Farsit-007 <public>
2024-08-17 04:31:01 +0600

    Updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const ProductsCollection = client.db('Jobtask').collection('products')
     const BransCollection = client.db('Jobtask').collection('brand')
     const CategoryCollection = client.db('Jobtask').collection('category')
